1476ea7a029a2c5b0caef51b0d5743d311a76fe9
cardosofiles <cardosofiles@outlook.com>
2024-11-25 22:36:34 -0300

    projeto com documentação do swagger

---
+++ src/routes.ts
@@ -0,0 +1,93 @@
+import { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
+import z from "zod";
+
+import { randomUUID } from "node:crypto";
+
+interface User {
+  id: string;
+  name: string;
+}
+
+const users: User[] = [];
+
+export const routes: FastifyPluginAsyncZod = async (app) => {
+  app.post(
+    "/users",
+    {
+      schema: {
+        tags: ["users"],
+        description: "Create a new user",
+        body: z.object({
+          name: z.string(),
+        }),
+        response: {
+          201: z.null(),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { name } = request.body;
+
+      users.push({
+        id: randomUUID(),
+        name,
+      });
+
+      return reply.status(201).send();
+    }
+  );
+
+  app.get(
+    "/users",
+    {
+      schema: {
+        tags: ["users"],
+        description: "List users",
+        response: {
+          200: z.array(
+            z.object({
+              id: z.string(),
+              name: z.string(),
+            })
+          ),
+        },
+      },
+    },
+    () => {
+      return users;
+    }
+  );
+
+  app.get(
+    "/users/:id",
+    {
+      schema: {
+        tags: ["users"],
+        description: "Get an users",
+        params: z.object({
+          id: z.string(),
+        }),
+        response: {
+          200: z.object({
+            id: z.string(),
+            name: z.string(),
+          }),
+          404: z.object({
+            message: z.string(),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { id } = request.params;
+
+      const user = users.find((user) => user.id === id);
+
+      if (!user) {
+        return reply.status(404).send({ message: "User not found" });
+      }
+
+      return user;
+    }
+  );
+};
---
+++ src/server.ts
@@ -0,0 +1,49 @@
+import { fastifyCors } from "@fastify/cors";
+import { fastifySwagger } from "@fastify/swagger";
+import { fastifySwaggerUi } from "@fastify/swagger-ui";
+import { fastify } from "fastify";
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from "fastify-type-provider-zod";
+
+import { routes } from "@/routes";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(fastifyCors),
+  {
+    origin: "*",
+    credentials: true,
+  };
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Typed full-stack API",
+      version: "1.0.0",
+      description: "A simple API using Fastify",
+    },
+
+    externalDocs: {
+      url: "https://swagger.io/docs/specification/external-docs/",
+      description: "Find more info here",
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+
+app.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
+});
+
+app.register(routes);
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("Server is running on port 3333");
+});
